12b3819027ff6260eb9b301b02c733d6ddd521c7
unknown <y.wilke@protonmail.com>
2021-05-18 00:08:48 +0200

    version 2

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 import os
+import sys
 import secrets
 import sqlite3 as sqlite
+import re
+import time
+import requests
 from validate_email import validate_email
 
-from flask import render_template, request, abort
+from flask import render_template, request, abort, redirect
 from flask_recaptcha import ReCaptcha
 
 from app import app
@@ -19,58 +23,101 @@ def create_table(db_file):
     with conn:
         cur.execute("""CREATE TABLE IF NOT EXISTS users(
                     user_id INTEGER PRIMARY KEY,
-                    region TEXT COLLATE NOCASE,
+                    postcode TEXT COLLATE NOCASE,
+                    lat REAL,
+                    long REAL,
+                    max_dist INTEGER,
                     email TEXT UNIQUE COLLATE NOCASE,
                     token INTEGER);""")
-        cur.execute('''CREATE INDEX IF NOT EXISTS idx_user_region 
-                    ON users (region);''')
+        cur.execute('''CREATE INDEX IF NOT EXISTS idx_lat_long ON users (lat, long);''')
+        #TODO index
 
 
-def valid_region(region):
-    region = region.strip()
-    region = region.lower()
-    if len(region) < 2 or len(region) > 50:
+def valid_postcode(postcode):
+    postcode = postcode.strip().replace(" ", "").upper()
+    postcode_regex = "[\d]{4}[A-Z]{2}"
+    match = re.fullmatch(postcode_regex, postcode)
+    if not match:
         return False
     else:
-        return region
+        return postcode
+
+def postcode_coordinate(postcode):
+    for i in range(3): # Retry 2 times
+        api = "https://bwnr.nl/postcode.php"
+        params = {"ak": bwnr_API_KEY,
+                  "pc": postcode,
+                  "ac": "pc2straat",
+                  "tg": "data"}
+        resp = requests.get(api, params=params)
+        resp = resp.content.decode().split(";")
+        if len(resp) == 4:
+            break
+        elif i == 2:
+            return None
+        else:
+            time.sleep(1)
+            
+    lat = round(float(resp[2]), 3)
+    long = round(float(resp[3]), 3)
+    return {"lat": lat, "long": long}
         
     
-def add_email(region, email, conn):
-    # Validate region and email
-    region = valid_region(region)
-    if region == False:
-        return "Plaatsnaam niet geldig"
+def add_email(postcode, email, max_dist, conn):
+    # Validate postcode and email
+    postcode = valid_postcode(postcode)
+    
+    if postcode == False:
+        return "Postcode niet geldig!"
+    
+    loc = postcode_coordinate(postcode)
+    if not loc:
+        return "Probeer een andere postcode in de buurt."
+    
     if validate_email(email) == False:
         return "Niet een geldig email adres!"
     
     token = secrets.randbelow(10**9) # Token to unsubscribe
     try:
         with conn:
-            conn.execute("""INSERT INTO users (region, email, token) VALUES (?,?,?)""", (region, email, token))
+            conn.execute("""INSERT INTO users (postcode, email, token, lat, long, max_dist) VALUES (?,?,?,?,?,?)""", (postcode, email, token, loc["lat"], loc["long"], max_dist))
     except sqlite.IntegrityError:
         return "Email staat al geregistreerd!"
     return True
 
 
 db_file = "vaccin_users.db"
+dist_dict = {"5km": 5000, "10km": 10000, "15km": 15000}
+bwnr_API_KEY = os.environ.get("bwnr_API_KEY", default=None)
+if not bwnr_API_KEY:
+    sys.exit("API KEY failed to load")
 conn = sqlite.connect(db_file, check_same_thread=False)
 cur = conn.cursor()
 create_table(db_file)
 
 
 # Routing
+@app.route('/', methods=['GET'])
+def root_page():
+    return redirect("/PrullenbakVaccin/aanmelden")
+
 @app.route('/PrullenbakVaccin/aanmelden', methods=['POST', 'GET'])
 def signup_page():
     if request.method == 'GET':
         return render_template('sign_up.html')
-    region = request.form["region"]
+    postcode = request.form["postcode"]
     email = request.form["email"]
-    inserted = add_email(region, email, conn)
+    max_dist = request.form["max_dist"]
+    try: # verify max_dist var and transform to meters
+        max_m = dist_dict[max_dist]
+    except KeyError:
+        max_m = 10000
+    
+    inserted = add_email(postcode, email, max_m, conn)
     if inserted != True:
-        return render_template('sign_up.html', region=region, email=email, error=inserted)
+        return render_template('sign_up.html', postcode=postcode, email=email, error=inserted)
     elif inserted == True:
-        return render_template('sign_up.html', region=region, email=email, success=True)
-    return render_template('sign_up.html', region=region, email=email)
+        return render_template('sign_up.html', postcode=postcode, email=email, success=True)
 
 
 @app.route('/PrullenbakVaccin/unsub', methods=['GET'])
--- vaccin_checker.py
+++ vaccin_checker.py
@@ -1,8 +1,11 @@
+from math import sqrt
 import time
 import pickle
 import os, sys
 import smtplib, ssl
 import datetime
+import re
+import sqlite3 as sqlite
 import requests
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -22,23 +25,11 @@ def write_log(message, also_print=True):
         print(message)
 
 
-def create_user_dict(file):
-    user_dict = {}
-    with open(file, "r") as user_f:
-        for line in user_f:
-            line = line.split("\t")
-            user_mail = line[0].strip()
-            user_region = line[1].strip()
-            try:
-                user_dict[user_region].append(user_mail)
-            except KeyError:
-                user_dict[user_region] = [user_mail,]
-    return user_dict
-
 def test_mail_login(context):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(gmail, password)
-    
+
+  
 def mail_result(resp, region, context):
     subs = user_dict[region]
     if test_run == True:
@@ -60,47 +51,115 @@ def mail_result(resp, region, context):
         server.sendmail(gmail, subs, message.as_string())
 
 
-def parse_site(location):
-    region_dict = {}
-    s = requests.Session()
-    r = s.get("https://www.prullenbakvaccin.nl/")
-    soup = BeautifulSoup(r.content, "html.parser")
-    token = soup.find("input", attrs={"name": "_token"})["value"]
-    payload = {"location": location, "_token": token}
-    r = s.post("https://www.prullenbakvaccin.nl/", data=payload)
+def parse_site():
+    postcode_regex = "[\d]{4}[A-Z]{2}"
+    now_avail = {}
+    
+    r = requests.get("https://www.prullenbakvaccin.nl/")
+    
+    if test_run == True:
+        r = pickle.load(open("test_pos_1.p", "rb"))
+    
     if r.status_code != 200:
-        sys.exit(f"HTTP code: {r.status_code}") #TODO log and notify
+        return {} #TODO log
     soup = BeautifulSoup(r.content, "html.parser")
-    soup = soup.find("div", attrs= {"id": "locations-container"})
-    resp = str(soup)
-    soup = soup.find_all("h5")
-    for x in soup:
-        locatie = x["id"]
-        status = x.find("small").string.extract().replace("\n", " ").replace(" ", "")
-        region_dict[locatie] = status.strip()
-    return region_dict, resp
+    soup = soup.find("div", attrs={"id": "locations-container"})
+    if not soup: return {} # No available locations
+    soup = soup.find_all("div", attrs={"class": "card mb-2"})
+    if not soup:
+        return {} #TODO log not expected
+    
+    for elem in soup: # Loop over all doctors now available
+        locatie_id = elem.find("h5")["id"]
+        if not locatie_id:
+            return {} #TODO log
+        locatie_text = str(elem.find("p", attrs={"class": "card-text"}))
+        match = re.search(postcode_regex, locatie_text)
+        if not match:
+            return {} #TODO log not expected
+        postcode = match.group(0)
+        now_avail[locatie_id] = {"postcode": postcode, "html_card": elem}
+    return now_avail
 
 
-def check_available(region_dict):
-    notify = False
-    vaccin_state = pickle.load(open("state.p", "rb")) # Read vaccine state
+def nearby_entries(coords):
+    conn = sqlite.connect(f"file:{db_file}?mode=ro", uri=True)
+    cur = conn.cursor()
+    lat = coords["lat"]; long = coords["long"]
+    lat_range = 0.18; long_range = 0.28 # +-20km
+    low_lat = lat - lat_range; high_lat = lat + lat_range
+    low_long = long - long_range; high_long = long + long_range
     
-    write_log(f"Result: {region_dict}")
-    write_log(f"State: {vaccin_state}")
+    cur.execute("""
+    SELECT email, token, max_dist, lat, long FROM users
+    WHERE lat BETWEEN ? AND ? AND long BETWEEN ? AND ?;
+    """, (low_lat, high_lat, low_long, high_long))
     
-    for locatie in region_dict:
-        if locatie in vaccin_state:
-            if vaccin_state[locatie] != region_dict[locatie]: # If different from last state
-                if region_dict[locatie] != neg_resp: # Maybe a vaccin available
-                    notify = True
-            vaccin_state[locatie] = region_dict[locatie]
+    entries = cur.fetchall()
+    conn.close()
+    return entries   
+
+
+def postcode_coordinate(postcode):
+    for i in range(3): # Retry 2 times
+        api = "https://bwnr.nl/postcode.php"
+        params = {"ak": bwnr_API_KEY,
+                  "pc": postcode,
+                  "ac": "pc2straat",
+                  "tg": "data"}
+        resp = requests.get(api, params=params)
+        resp = resp.content.decode().split(";")
+        if len(resp) == 4:
+            break
+        elif i == 2:
+            return None
         else:
-            if region_dict[locatie] != neg_resp: # Maybe a vaccin available
-                notify = True
-            vaccin_state[locatie] = region_dict[locatie] # Set state for new location
+            time.sleep(1)
+            
+    lat = round(float(resp[2]), 2)
+    long = round(float(resp[3]), 2)
+    return {"lat": lat, "long": long}
+
+
+def filter_distance(entries, coords):
+    return_entries = []
+    for entry in entries:
+        lat_dist = abs(entry[3] - coords["lat"]) * 110 * 1000 # Calculatee lat dist in m
+        long_dist = abs(entry[4] - coords["long"]) * 70 * 1000 # Calcualte long dist in m
+        dist = sqrt(lat_dist**2 + long_dist**2)
+        print(dist)
+        if dist < entry[2]:
+            list(entry).append(dist)
+            return_entries.append(entry)
+    return return_entries
+
+
+def find_nearby_email(new_locs):
+    return_locs = []
+    for loc in new_locs:
+        coords = postcode_coordinate(loc["postcode"])
+        if not coords:
+            continue #TODO log
+        entries = nearby_entries(coords)
+        entries = filter_distance(entries, coords)
+        loc["users"] = entries # entries = (email, token, max_dist, lat, long, dist)
+        return_locs.append(loc)       
+    return return_locs
+
+
+def process_changes(avail_state, now_avail):
+    new_locs = []
+    for locatie_id in now_avail: 
+        if locatie_id not in avail_state: # check if available location is new
+            avail_state.add(locatie_id) # add to known available locations
+            now_avail[locatie_id]["id"] = locatie_id
+            new_locs.append(now_avail[locatie_id])
+            
+    for locatie_id in avail_state: # remove available locations from state that are no longer available
+        if locatie_id not in now_avail:
+            avail_state.remove(locatie_id)
     
-    pickle.dump(vaccin_state, open("state.p", "wb")) # Write vaccin state
-    return notify
+    return avail_state, new_locs
 
 
 def main():
@@ -108,19 +167,32 @@ def main():
     # Setup TLS for email
     context = ssl.create_default_context()
     test_mail_login(context)
-    
-    # Load previous state
-    if os.path.isfile("state.p"):
-        vaccin_state = pickle.load(open("state.p", "rb"))
-    else:   
-        vaccin_state = dict()
-        pickle.dump(vaccin_state, open("state.p", "wb"))
-    
+
+    # Check for changes in loop
+    avail_state = set()
+    while True:
+        now_avail = parse_site()
+        #print(f"now avail: {now_avail}")
+        avail_state, new_locs = process_changes(avail_state, now_avail)
+        #print(f"new_locs: {new_locs}")
+        new_locs = find_nearby_email(new_locs)
+            
+        for i in range(wait_time):
+            time.sleep(1)
+
+    """
     # Check for updates in loop
+    doctor_dict = {}
     while True:
         # Test run
         if test_run == True:
             write_log("!!!TEST RUN!!!")
+        
+        # Parse site
+        doctors = parse_site(driver)
+        doctor_dict = process_changes(doctor_dict, doctors)
+        
+        
         # Check al user regions
         for region in user_dict:
             # Get data for location
@@ -137,19 +209,21 @@ def main():
                 write_log("Nothing to report")
             for i in range(int(wait_time / (len(user_dict)))):
                 time.sleep(1)
+        """
 
 
-# Users dict
-user_dict = create_user_dict("users.txt")
 
 # Setup vars
 neg_resp = "Heeftgeenvaccins"
 pos_resp = "Heeftvaccinsbeschikbaar"
+db_file = "vaccin_users.db"
 wait_time = 300 # sec
 port = 465  # For SSL
 gmail = "dev.yanowilke@gmail.com"
 contact_mail = "dev.yanowilke@gmail.com"
 password = input("Password: ")
-test_run = False
-
+test_run = True
+bwnr_API_KEY = os.environ.get("bwnr_API_KEY", default=None)
+if not bwnr_API_KEY:    
+    sys.exit("API KEY failed to load")
 main()
